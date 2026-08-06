96b0e1501b0881a44146baaffdf3e47b26f1f617
kdrabha2000-hue <kdrabha2000@gmail.com>
2026-08-06 21:37:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 
 
 let cart = [];
+let total = 0;
+
+const prices = {
+  "Chicken Biryani": 180,
+  "Veg Thali": 120,
+  "Fried Rice": 100,
+  "Chicken Roll": 90,
+  "Veg Roll": 50
+};
 
 function addToCart(itemName) {
-    cart.push(itemName);
+  cart.push(itemName);
+  total += prices[itemName];
 
-    let cartBox = document.getElementById("cart");
-
-    if (cartBox) {
-        cartBox.innerHTML =
-            "<h2>🛒 Cart</h2>" +
-            cart.map(item => "<p>" + item + "</p>").join("");
-    }
+  document.getElementById("cart").innerHTML =
+    "<h2>🛒 Cart</h2>" +
+    cart.map(item => "<p>" + item + "</p>").join("") +
+    "<hr><h3>Total: ₹" + total + "</h3>";
 }
